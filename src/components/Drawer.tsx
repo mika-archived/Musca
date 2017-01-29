3e3204f67598669e91715a57efbf04299e2a45d5
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2017-01-29 18:50:45 +0900

    :shirt: Fix code styles.

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Drawer as RebassDrawer, Panel, Text} from "rebass";
+import { Drawer as RebassDrawer } from "rebass";
 import { DrawerMenu } from "./DrawerMenu";
 
 interface IDrawerProps {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Icon from "react-geomicons";
 import { Link } from "react-router";
-import { Toolbar, NavItem, Space } from "rebass";
+import { NavItem, Space, Toolbar } from "rebass";
 import { Drawer } from "./Drawer";
 
 interface IHeaderStates {
@@ -17,8 +17,8 @@ export class Header extends React.Component<void, IHeaderStates> {
     return (
       <div>
         <Toolbar>
-          <NavItem is={'a'} {...{onClick: this.onClickIcon.bind(this), style: {width: "48px"}}}>
-            <Icon name="list" 
+          <NavItem is={"a"} {...{onClick: this.onClickIcon.bind(this), style: {width: "48px"}}}>
+            <Icon name="list"
               style={{height: "28px", width: "28px"}} />
           </NavItem>
           <Space
